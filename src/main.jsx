25643d4968091bdd167167a7df8be16e786b6492
sami855-ux <samitale56@outlook.com>
2024-12-24 13:46:10 +0500

    the context for the book is connected

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
+import { BookContextProvider } from "./Context/BookContext.jsx"
 import "./index.css"
 import App from "./App.jsx"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BookContextProvider>
+      <App />
+    </BookContextProvider>
   </StrictMode>
 )

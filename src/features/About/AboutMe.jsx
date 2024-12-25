c986adf98b717ce607a6891005b13b47b32b22e3
sami855-ux <samitale56@outlook.com>
2024-12-25 10:17:00 +0500

    About page is finished

--- src/features/About/AboutMe.jsx
+++ src/features/About/AboutMe.jsx
@@ -1,3 +1,6 @@
+import { FaGithub, FaLinkedin, FaTelegram } from "react-icons/fa"
+import PropTypes from "prop-types"
+
 const AboutMe = () => {
   return (
     <div className="w-full min-h-[85vh] flex items-center justify-center space-x-4 px-20">
@@ -16,10 +19,35 @@ const AboutMe = () => {
           animi.
         </p>
 
-        <div className="flex space-x-3"></div>
+        <div className="flex space-x-3 mt-7">
+          <Icon
+            icon={<FaGithub className="text-lg" size={20} />}
+            link="https://github.com/sami855-ux"
+          />
+          <Icon
+            icon={<FaLinkedin className="text-lg" size={20} />}
+            link="https://www.linkedin.com/in/sami-tale-49aa79237/"
+          />
+          <Icon
+            icon={<FaTelegram className="text-lg" size={20} />}
+            link="https://t.me/Sami_hhtt"
+          />
+        </div>
       </section>
     </div>
   )
 }
 
+const Icon = ({ icon, link }) => {
+  return (
+    <a href={link} className="cursor-pointer " target="_blank">
+      {icon}
+    </a>
+  )
+}
+
+Icon.propTypes = {
+  icon: PropTypes.object.isRequired,
+  link: PropTypes.string.isRequired,
+}
 export default AboutMe
